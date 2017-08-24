57be3fe638ca407eb4e956507b8090f32831ae4a
Lewin Bormann <lbo@spheniscida.de>
2017-08-24 20:09:47 +0200

    types: Completely remodel iterator type.

--- src/types.rs
+++ src/types.rs
@@ -21,13 +21,13 @@ pub struct Range<'a> {
 /// An extension of the standard `Iterator` trait that supports some methods necessary for LevelDB.
 /// This works because the iterators used are stateful and keep the last returned element.
 ///
-/// Note: Implementing types are expected to hold `!valid()` before the first call to `next()`.
-pub trait LdbIterator: Iterator {
-    // We're emulating LevelDB's Slice type here using actual slices with the lifetime of the
-    // iterator. The lifetime of the iterator is usually the one of the backing storage (Block,
-    // MemTable, SkipMap...)
-    // type Item = (&'a [u8], &'a [u8]);
-
+/// Note: Implementing types are expected to hold `!valid()` before the first call to `advance()`.
+pub trait LdbIterator {
+    /// advance advances the position of the iterator by one element (which can be retrieved using
+    /// current(). If no more elements are available, advance() returns false.
+    fn advance(&mut self) -> bool;
+    /// Return the current item (i.e. the item most recently returned by get_next())
+    fn current(&self, key: &mut Vec<u8>, val: &mut Vec<u8>) -> bool;
     /// Seek the iterator to `key` or the next bigger key. If the seek is invalid (past last
     /// element), the iterator is reset() and not valid.
     /// After a seek to an existing key, current() returns that entry.
@@ -36,14 +36,39 @@ pub trait LdbIterator: Iterator {
     fn reset(&mut self);
     /// Returns true if `current()` would return a valid item.
     fn valid(&self) -> bool;
-    /// Return the current item (i.e. the item most recently returned by next())
-    fn current(&self) -> Option<Self::Item>;
     /// Go to the previous item. This is inefficient for most iterators.
-    fn prev(&mut self) -> Option<Self::Item>;
+    fn prev(&mut self) -> bool;
 
+    // default implementations.
+
+    /// next is like Iterator::next(). It's implemented here because "only traits defined in the
+    /// current crate can be implemented for a type parameter" (says rustc).
+    fn next(&mut self) -> Option<(Vec<u8>, Vec<u8>)> {
+        if !self.advance() {
+            return None;
+        }
+        let (mut key, mut val) = (vec![], vec![]);
+        if self.current(&mut key, &mut val) {
+            Some((key, val))
+        } else {
+            None
+        }
+    }
+    /// seek_to_first seeks to the first element.
     fn seek_to_first(&mut self) {
         self.reset();
-        self.next();
+        self.advance();
+    }
+}
+
+/// current_key_val is a helper allocating two vectors and filling them with the current key/value
+/// of the specified iterator.
+pub fn current_key_val<It: LdbIterator>(it: &It) -> Option<(Vec<u8>, Vec<u8>)> {
+    let (mut k, mut v) = (vec![], vec![]);
+    if it.current(&mut k, &mut v) {
+        Some((k, v))
+    } else {
+        None
     }
 }
 
